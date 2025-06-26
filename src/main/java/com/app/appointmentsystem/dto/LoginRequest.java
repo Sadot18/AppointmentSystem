305d258dfd51d07e7aaa7447bc22520f44ddebbe
Sadot18 <19161291@itoaxaca.edu.mx>
2025-06-26 14:52:56 -0600

    Añade objetos de transferencia de datos para la autenticación
    gestionar los datos de autenticación y registro

---
+++ src/main/java/com/app/appointmentsystem/dto/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.app.appointmentsystem.dto;
+
+public record LoginRequest(String email, String password) {
+
+}
---
+++ src/main/java/com/app/appointmentsystem/dto/SignupRequest.java
@@ -0,0 +1,8 @@
+package com.app.appointmentsystem.dto;
+
+public record SignupRequest(
+        String email,
+        String password,
+        String name
+) {
+}
